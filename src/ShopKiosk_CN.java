b08e3fbcf8d1d0aa14699cb59ee892f153a9e004
dgjPolyTech <dgjpolytech@gmail.com>
2025-06-29 15:26:45 +0900

    CN, JP, NY 선언

---
+++ src/ShopKiosk_CN.java
@@ -0,0 +1,16 @@
+public class ShopKiosk_CN extends ShopKiosk {
+    int idx = 0;
+
+    public ShopKiosk_CN() {
+        this.shopName = "백일각";
+        addMenu("짜장면", 1, 5000);
+        addMenu("짬뽕", 2, 6000);
+        addMenu("탕수육", 3, 13000);
+    }
+
+    public ShopKiosk_CN(String mName, int mPrice) {
+        this.shopName = "백일각";
+        idx++;
+        addMenu(mName, idx, mPrice);
+    }
+}
---
+++ src/ShopKiosk_JP.java
@@ -0,0 +1,16 @@
+public class ShopKiosk_JP extends ShopKiosk{
+    int idx = 0;
+
+    public ShopKiosk_JP() {
+        this.shopName = "카츠 앤 스시";
+        addMenu("우동", 1, 6500);
+        addMenu("돈까스", 2, 9500);
+        addMenu("스시 정식", 3, 12000);
+    }
+
+    public ShopKiosk_JP(String mName, int mPrice) {
+        this.shopName = "카츠 앤 스시";
+        idx++;
+        addMenu(mName, idx, mPrice);
+    }
+}
